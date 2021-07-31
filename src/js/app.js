69fd85982c03c52188f28e7cf3d933736e9e1868
Samuel Gonzalez <xame2005@gmail.com>
2021-07-30 22:39:31 -0500

    [CREATED] Pagination buttons and conditional css rendering

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,58 @@
+let pagina = 1;
+
 document.addEventListener("DOMContentLoaded", function () {
   iniciarApp();
 });
 
 function iniciarApp() {
   mostrarServicios();
+
+  //Resalta el tab actual
+  mostrarSeccion();
+
+  //Oculta o muestra una sección según el tab que se presiona
+  cambiarSeccion();
+
+  //Paginacion siguiente y anterior
+  paginaSiguiente();
+  paginaAnterior();
+}
+
+function mostrarSeccion() {
+  const seccionActual = document.querySelector(`#paso-${pagina}`);
+  seccionActual.classList.add("mostrar-seccion");
+  //Resalta el tab actual
+  const tab = document.querySelector(`[data-paso="${pagina}"]`);
+  tab.classList.add("actual");
+}
+
+function cambiarSeccion() {
+  const enlaces = document.querySelectorAll(".tabs button");
+
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      pagina = parseInt(e.target.dataset.paso);
+
+      //Elimina mostrar-seccion de la sección anterior
+      document
+        .querySelector(".mostrar-seccion")
+        .classList.remove("mostrar-seccion");
+
+      //Eliminar la clase .actual del tab anterior
+      document.querySelector(".tabs .actual").classList.remove("actual");
+
+      //Agregar la clase actual al nuevo tab
+      const tab = document.querySelector(`[data-paso="${pagina}"]`);
+      tab.classList.add("actual");
+
+      const seccion = document.querySelector(`#paso-${pagina}`);
+      seccion.classList.add("mostrar-seccion");
+
+      // Llamar la función de mostrar sección
+    });
+  });
 }
 
 async function mostrarServicios() {
@@ -33,6 +82,9 @@ async function mostrarServicios() {
       const servicioDiv = document.createElement("DIV");
       servicioDiv.classList.add("servicio");
 
+      //Selecciona un servicio para la cita
+      servicioDiv.onclick = seleccionarServicio;
+
       //Inyectar Precio y Nombre al div de servicio
       servicioDiv.appendChild(nombreServicio);
       servicioDiv.appendChild(precioServicio);
@@ -41,4 +93,34 @@ async function mostrarServicios() {
       document.querySelector("#servicios").appendChild(servicioDiv);
     });
   } catch (error) {}
+}
+
+function seleccionarServicio(e) {
+  let elemento;
+  //Obligar que el elemento al que se le da click sea una etiqueta <div></div>
+  if (e.target.tagName === "P") {
+    elemento = e.target.parentElement;
+  } else {
+    elemento = e.target;
+  }
+
+  if (elemento.classList.contains("seleccionado")) {
+    elemento.classList.remove("seleccionado");
+  } else {
+    elemento.classList.add("seleccionado");
+  }
+}
+
+function paginaSiguiente() {
+  document.querySelector("#siguiente");
+  paginaSiguiente.addEventListener("click", () => {
+    pagina++;
+  });
+}
+
+function paginaAnterior() {
+  document.querySelector("#anterior");
+  paginaSiguiente.addEventListener("click", () => {
+    pagina--;
+  });
 }
